3797080dcd8bb6b4f4749256977d358a4ec50f32
Martin Vančo <vancoweb.design@gmail.com>
2025-12-29 16:08:18 +0100

    removed dummy files

--- composeApp/src/commonMain/kotlin/sk/martinvanco/monad/wifi_test_v2/presentation/WifiTestV2State.kt
+++ composeApp/src/commonMain/kotlin/sk/martinvanco/monad/wifi_test_v2/presentation/WifiTestV2State.kt
@@ -1,8 +1,8 @@
 package sk.martinvanco.monad.wifi_test_v2.presentation
 
 data class WifiTestV2State(
-    val ssid: String = "Veverka Devolo",
-    val password: String = "Vancik1234",
+    val ssid: String = "",
+    val password: String = "",
     val isConnecting: Boolean = false,
     val statusMessage: String = "",
     val currentNetwork: String? = null,
